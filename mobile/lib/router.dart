fb6068fcf1015775c9fc3aa40e3ee2b3fe2e9513
AxelYagami <axeloc@gmail.com>
2026-03-04 14:24:06 -0600

    Fix ios notificaciones

--- mobile/lib/router.dart
+++ mobile/lib/router.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+import 'package:flutter/scheduler.dart' hide Priority;
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
